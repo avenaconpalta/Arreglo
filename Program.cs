95e758cc04f63a27541865e504b51ad012a18573
avenaconpalta <willtbtxd@gmail.com>
2024-11-13 23:24:29 -0500

    subir arreglo

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el tamaño del arreglo: ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> valores = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Ingrese el valor en la posición {i}: ");
+            valores.Add(int.Parse(Console.ReadLine()));
+        }
+        Console.WriteLine("Arreglo inicial:");
+        MostrarArreglo(valores);
+
+        Console.Write("Ingrese la posición a eliminar: ");
+        int posicion = int.Parse(Console.ReadLine());
+
+      
+        if (posicion >= 0 && posicion < valores.Count)
+        {
+            
+            valores.RemoveAt(posicion);
+
+            
+            Console.WriteLine("Arreglo después de eliminar:");
+            MostrarArreglo(valores);
+        }
+        else
+        {
+            Console.WriteLine("Posición inválida.");
+        }
+    }
+
+    static void MostrarArreglo(List<int> arreglo)
+    {
+        foreach (int valor in arreglo)
+        {
+            Console.Write(valor + " ");
+        }
+        Console.WriteLine();
+    }
+}
